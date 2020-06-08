4e4b997eb48fac51ba4a5310ac7e6fb091efb522
Raj Soni <rajsoni@readybytes.in>
2020-06-08 19:15:16 +0530

    added-dependency-vuex-persistance

--- vuejs/src/store/index.js
+++ vuejs/src/store/index.js
@@ -1,14 +1,23 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import createPersistedState from 'vuex-persistedstate';
 
 // const loadModule = (module) => () => import(`@/store/modules/${module}`);
 // const app = loadModule('app');
 import app from './modules/app';
 import auth from './modules/auth';
+import task from './modules/task';
+import user from './modules/user';
+
 Vue.use (Vuex);
 
 export default new Vuex.Store ({
   state: {},
   actions: {},
-  modules: {app, auth},
+  modules: {app, auth, task, user},
+  plugins: [
+    createPersistedState ({
+      key: 'LaraVue',
+    }),
+  ],
 });
