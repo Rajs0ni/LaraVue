809ca63f7e9772d6b4bd923aefb7c27798f6dca2
Raj Soni <rajsoni@readybytes.in>
2020-06-11 21:11:43 +0530

    Align content in center

--- vuejs/src/pages/member/task/List.vue
+++ vuejs/src/pages/member/task/List.vue
@@ -1,14 +1,12 @@
 <template>
-  <div>
-    <v-row align="center" justify="center">
-      <v-col cols="12">
-        <div class="text-center">To create a new task, click + at the bottom of your screener</div>
-      </v-col>
+  <v-container>
+    <v-row justify="center" align="center">
+      <div class="text-center">To create a new task, click + at the bottom of your screener</div>
     </v-row>
     <v-btn fab dark fixed right bottom color="customPrimary">
       <v-icon>$vuetify.icons.ap_add</v-icon>
     </v-btn>
-  </div>
+  </v-container>
 </template>
 
 <script>
