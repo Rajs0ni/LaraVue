07db1e4e5f12f7ebf382a020f7aecff26d284fe6
Raj Soni <rajsoni@readybytes.in>
2020-06-14 22:02:25 +0530

    integrated momentjs

--- vuejs/src/main.js
+++ vuejs/src/main.js
@@ -5,9 +5,11 @@ import router from '@/router';
 import store from '@/store';
 import DEFINES from '@/defines';
 import vuetify from '@/plugins/vuetify';
+import moment from 'moment';
 
 Vue.config.productionTip = false;
 Vue.prototype.DEFINES = DEFINES;
+Vue.prototype.moment = moment;
 
 new Vue({
   router,
